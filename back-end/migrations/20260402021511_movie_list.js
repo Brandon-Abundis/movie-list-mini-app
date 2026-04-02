c54982110a62ac605122cd6cabc3b109d543ade6
Brandon-Abundis <abrandon99@gmail.com>
2026-04-01 21:02:19 -0600

    docker compose and DB seeding

---
+++ back-end/migrations/20260402021511_movie_list.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('movie_list', table => {
+    table.increments();
+    table.string('title');
+    table.string('genres');
+    table.string('description')
+    table.string('main_character');
+    table.date('year_released'); //YYYY-MM-DD
+    table.string('poster');
+
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('movie_list');
+};
